86f1e13655ccc26bafa104d8886a5fc9632b4d4d
Kanishk Pachauri <itskanishkp.py@gmail.com>
2023-07-18 18:57:36 +0530

    Updated Data

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+import uvicorn
+from fastapi import FastAPI, HTTPException
 import sqlite3
 import time
-from files_download import save_out
 from bs4 import BeautifulSoup
 import re
-app = Flask(__name__)
+
+app = FastAPI()
 
 # Create a database and a table
 conn = sqlite3.connect('sensor_data.db')
@@ -19,13 +20,8 @@ conn.execute('''CREATE TABLE IF NOT EXISTS sensor_data
 print("Table created successfully")
 conn.close()
 
-save_out(url = "http://192.168.110.245/")
-
-# Define a function to parse the HTML file and insert data into the database
-def insert_sensor_data():
-    with open('sensor_data.html', 'r') as f:
-        content = f.read()
-
+# Instead of saving the file during app initialization, you can define the function to parse HTML and insert data.
+def parse_and_insert_data(content):
     soup = BeautifulSoup(content, 'html.parser')
 
     temperature = soup.find_all('p')[1].text
@@ -37,8 +33,6 @@ def insert_sensor_data():
     # Insert the data into the database
     conn = sqlite3.connect('sensor_data.db')
     print("Opened database successfully")
-    # conn.execute(f"INSERT INTO sensor_data (TEMPERATURE, HUMIDITY, X_ACC, Y_ACC, Z_ACC) \
-    #              VALUES ({str(temperature)}, {str(humidity)}, {str(x_acc)}, {str(y_acc)}, {str(z_acc)})")
     conn.execute("INSERT INTO sensor_data (TEMPERATURE, HUMIDITY, X_ACC, Y_ACC, Z_ACC) VALUES (?, ?, ?, ?, ?)",
                  (temperature, humidity, x_acc, y_acc, z_acc))
     conn.commit()
@@ -46,8 +40,8 @@ def insert_sensor_data():
     conn.close()
 
 # Define a route to retrieve the data from the database
-@app.route('/sensor-data')
-def get_sensor_data():
+@app.get('/sensor-data')
+async def get_sensor_data():
     # Connect to the database
     conn = sqlite3.connect('sensor_data.db')
 
@@ -65,11 +59,18 @@ def get_sensor_data():
                             'x_acc': row[3], 'y_acc': row[4], 'z_acc': row[5]})
 
     # Return the data as a JSON response
-    return jsonify(sensor_data)
+    return sensor_data
+
+# Since FastAPI is asynchronous, we can use a background task to periodically fetch data and insert it into the database.
+async def background_task():
+    while True:
+        with open('sensor_data.html', 'r') as f:
+            content = f.read()
+        parse_and_insert_data(content)
+        await asyncio.sleep(0.1)
 
 if __name__ == '__main__':
-    while True:
-        insert_sensor_data()
-        # REALOAD PAGE AT EVERY 100MS
-        time.sleep(0.1)
-        app.run(debug=True)
+    import asyncio
+    loop = asyncio.get_event_loop()
+    loop.create_task(background_task())
+    uvicorn.run(app, host='0.0.0.0', port=8000)
